186aa270722d413e041b4ee389e95aa63758b418
ZhiHao Qian <lacusrinz@gmail.com>
2017-01-15 22:30:43 +0800

    update

---
+++ Save Shanghai/Assets/_Scripts/Gun.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+    public Rigidbody2D rocket;
+    public float speed;
+
+    private Animator anim;
+
+	void Awake () {
+        anim = transform.root.gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            anim.SetTrigger("Shoot");
+        }
+	}
+}
